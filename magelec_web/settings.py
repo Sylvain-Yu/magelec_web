9bb90173c213ca691d8f3ca1a13afb188e3f7a53
Sylvain-Yu <jasonshake@163.com>
2019-07-12 16:14:25 +0800

    add someting more ,such as highspeed,torque vs current

--- magelec_web/settings.py
+++ magelec_web/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'magelec_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
         'USER': 'root',
         'PASSWORD': 'toor1234',
--- test_app/admin.py
+++ test_app/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
 from . import models
 # Register your models here.
-admin.site.register(models.MotorInfo)
-admin.site.register(models.HighSpeed)
+
+class MotorSimpleInfo(admin.ModelAdmin):
+
+    list_display = ['motor_PN','motor_model','motor_code','highspeed','c_time','comment']
+    search_fields = ['motor_PN','motor_model','motor_code']
+    date_hierarchy = 'c_time'
+    list_per_page = 50
+    empty_value_display = '-空白-'
+    list_filter = ['motor_PN','motor_model']
+    fk_fields = 'highspeed'
+
+class HighSpeedInfo(admin.ModelAdmin):
+    list_display = ['speed','passornot','c_time','comment']
+    empty_value_display = '-空白-'
+    date_hierarchy = 'c_time'
+
+class TorqueVsCurrentInfo(admin.ModelAdmin):
+    list_display = ['speed_point','rotate_direction','control_mode','c_time','comment']
+    empty_value_display = '-空白-'
+    date_hierarchy = 'c_time'
+
+admin.site.site_header = 'MAGELEC 信息管理系统'
+admin.site.site_title = 'MAGELEC'
+admin.site.register(models.MotorInfo,MotorSimpleInfo)
+admin.site.register(models.HighSpeed,HighSpeedInfo)
+admin.site.register(models.CurrentVsTorque,TorqueVsCurrentInfo)
