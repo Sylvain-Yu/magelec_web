a3deef938daf6152ab37469b9c7dcdf38b6d2d4b
root <root@vultr.guest>
2019-07-19 07:02:09 +0000

    publish version -0.01

--- magelec_web/settings.py
+++ magelec_web/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ohe5qbyg(easyeo2)+*-e)s8tm&*9*0^u%wveul3gq#m#t7_r_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['www.sylvain.tech']
 
 
 # Application definition
